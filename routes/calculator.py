25c4322cff62142a2b0378572fd5256a04c326e4
marco-gallegos <ma_galeza@hotmail.com>
2023-02-09 01:07:40 -0600

    chore(calculator): new endpoint with calculator

--- routes/calculator.py
+++ routes/calculator.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
+from calculator.calculator import calculate_ebay_costs, currency_against_dollar
 
 app = APIRouter()
 
 @app.get("/calculator/{price}")
-async def read_user(price: float, dollar_price: float = 1):
-    return { "price": price, "dollar_price": dollar_price }
+async def read_user(price: float, dollar_price: float = None):
+    dollar_price_to_use = dollar_price if dollar_price is not None else currency_against_dollar['peso']
+    ebay_costs = calculate_ebay_costs(price, dollar_price_to_use)
+    print(ebay_costs)
+    return ebay_costs
