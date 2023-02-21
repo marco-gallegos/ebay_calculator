be5054c2a582888c9dea0fca9dfa9a025436e623
marco-gallegos <ma_galeza@hotmail.com>
2023-02-21 00:59:19 -0600

    chore(calculator): api param update

--- routes/calculator.py
+++ routes/calculator.py
@@ -4,8 +4,8 @@ from calculator.calculator import calculate_ebay_costs, currency_against_dollar
 app = APIRouter()
 
 @app.get("/calculator/{price}")
-async def read_user(price: float, dollar_price: float = None):
-    dollar_price_to_use = dollar_price if dollar_price is not None else currency_against_dollar['peso']
+async def read_user(price: float, dollar_price: float = -1):
+    dollar_price_to_use = dollar_price if dollar_price is not -1 and dollar_price > 5 else currency_against_dollar['peso']
     ebay_costs = calculate_ebay_costs(price, dollar_price_to_use)
     print(ebay_costs)
     return ebay_costs
